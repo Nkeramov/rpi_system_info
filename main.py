4eb71b8fcd8ef56cb0ece5e632623983c269331d
Nizam <keramov-nizam@yandex.ru>
2024-01-20 07:31:43 +0500

    refactored with using PiSystemInfo class

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
-import fcntl
-import struct
-import socket
-import platform
-import subprocess
+import logging
 from datetime import datetime
+from dotenv import load_dotenv
+
+from libs.logging_utils import get_logger
+
+load_dotenv('.env')
+
+LOGS_PATH = os.getenv("LOGS_PATH")
+LOG_LEVEL = os.getenv("LOG_LEVEL")
+LOG_MSG_FORMAT = os.getenv("LOG_MSG_FORMAT")
+LOG_DATE_FORMAT = os.getenv("LOG_DATE_FORMAT")
+LOG_FILENAME = os.path.join(LOGS_PATH, os.getenv("LOG_FILENAME"))
+logger = get_logger(LOG_LEVEL, LOG_MSG_FORMAT, LOG_DATE_FORMAT, LOG_FILENAME)
+
+
+from libs.pi_system_info import PiSystemInfo
+
+pi_sys_info = PiSystemInfo(logger)
 
 from flask import Flask, render_template
 from flask_caching import Cache
@@ -21,92 +34,11 @@ app.config.from_mapping(config)
 cache = Cache(app)
 
 
-
-def get_mac_address(interface='eth0'):
-    """
-	Return the MAC address of the specified interface
-    """
-    try:
-        str = open('/sys/class/net/%s/address' %interface).read()
-    except:
-        str = "00:00:00:00:00:00"
-    return str[0:17]
-
-
-def get_ip_address(interface='eth0'):
-    """
-	Return the IP address of the specified interface
-    """
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sockfd = s.fileno()
-        SIOCGIFADDR = 0x8915
-        ifreq = struct.pack('16sH14s', interface.encode('utf-8'), socket.AF_INET, b'\x00'*14)
-        res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
-        ip = socket.inet_ntoa(struct.unpack('16sH2x4s8x', res)[2])
-    except:
-        ip = "0.0.0.0"
-    return ip
-
-
-def get_pi_model():
-    pi_model = subprocess.check_output("cat /sys/firmware/devicetree/base/model", shell=True).decode().replace('\"', '')
-    return pi_model
-
-
-def cpu_generic_details():
-    try:
-        items = [s.decode().split('\t: ') for s in subprocess.check_output(["cat /proc/cpuinfo  | grep 'model name\|Hardware\|Serial' | uniq "], shell=True).splitlines()]
-    except Exception as ex:
-        print(ex)
-    finally:
-        return items
-
-
-def disk_usage_list():
-    try:
-        items = [s.split() for s in subprocess.check_output(['df', '-h'], universal_newlines=True).splitlines()]
-    except Exception as ex:
-        print(ex)
-    finally:
-        return items[1:]
-
-
-def running_process_list():
-    items = []
-    try:
-        items = [s.decode().split() for s in subprocess.check_output(["ps -Ao user,pid,pcpu,pmem,comm,lstart --sort=-pcpu"], shell=True).splitlines()]
-    except Exception as ex:
-        print(ex)
-    finally:
-        return items[1:]
-
-
 @app.route('/')
 # cached for 60 seconds
 @cache.cached(timeout=60)
 def index():
-    sys_data = {"current_time": '',"machine_name": ''}
-    try:
-        sys_data['current_time'] = datetime.now().strftime("%d-%b-%Y , %I : %M : %S %p")
-        sys_data['machine_name'] =  platform.node()
-        pi_model = get_pi_model()
-        ip_address = get_ip_address()
-        mac_address = get_mac_address()
-        cpu_generic_info = cpu_generic_details()
-        disk_usage_info = disk_usage_list()
-        running_process_info = running_process_list()
-    except Exception as ex:
-        print(ex)
-    finally:
-        return render_template("index.html", title='Raspberry Pi System Info',
-                                sys_data = sys_data,
-                                pi_model = pi_model,
-                                ip_address = ip_address,
-                                mac_address = mac_address,
-                                cpu_generic_info = cpu_generic_info,
-                                disk_usage_info= disk_usage_info,
-                                running_process_info = running_process_info)
+    return render_template("index.html", title='Raspberry Pi System Info')
 
 
 @app.route('/restart')
@@ -120,82 +52,110 @@ def shutdown():
 
 
 @app.context_processor
-def boot_info():
-    item = {'start_time': 'Na','running_since':'Na'}
-    try:
-        item['running_duration'] = subprocess.check_output(['uptime -p'], shell=True).decode()[3:]
-        item['start_time'] = subprocess.check_output(['uptime -s'], shell=True).decode()
-    except Exception as ex:
-        print(ex)
-    finally:
-        return dict(boot_info = item)
+def uptime_since():
+    t = datetime.strptime(pi_sys_info.get_uptime_since(), "%Y-%m-%d %H:%M:%S")
+    return dict(uptime_since=t.strftime("%d-%b-%Y, %I : %M : %S"))
 
 
 @app.context_processor
-def memory_usage_info():
-    try:
-        item = {'total': 0,'used': 0, 'free': 0,'available': 0 }
-        item['total'] = subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $2'}"], shell=True).decode()
-        item['used'] = subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $3'}"], shell=True).decode()
-        item['free'] = subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $4'}"], shell=True).decode()
-        item['cache'] = subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $6'}"], shell=True).decode()
-        item['available'] = subprocess.check_output(["free -m -t | awk 'NR==2' | awk '{print $7'}"], shell=True).decode()
-    except Exception as ex:
-        print(ex)
-    finally:
-        return dict(memory_usage_info = item)
+def uptime_pretty():
+    return dict(uptime_pretty=pi_sys_info.get_uptime_pretty())
 
 
 @app.context_processor
-def os_name():
-    os_info = subprocess.check_output("cat /etc/*-release | grep PRETTY_NAME | cut -d= -f2", shell=True).decode().replace('\"', '')
-    return dict(os_name=os_info)
+def current_time():
+    return dict(current_time=datetime.now().strftime("%d-%b-%Y, %I : %M : %S"))
 
 
 @app.context_processor
-def cpu_usage_info():
-    item = {'in_use': 0}
-    try:
-        item['in_use'] = subprocess.check_output("top -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4 }'", shell=True).decode()
-    except Exception as ex:
-        print(ex)
-    finally:
-        return dict(cpu_usage_info = item)
+def ip_address():
+    return dict(ip_address=pi_sys_info.get_ip_address())
 
 
 @app.context_processor
-def cpu_processor_count():
-    proc_info = subprocess.check_output("nproc", shell=True).decode().replace('\"', '')
-    return dict(cpu_processor_count=proc_info)
+def mac_address():
+    return dict(mac_address=pi_sys_info.get_mac_address().upper())
+
+
+@app.context_processor
+def cpu_model_name():
+    return dict(cpu_model_name=pi_sys_info.get_cpu_model_name())
+
+
+@app.context_processor
+def cpu_hardware_type():
+    return dict(cpu_hardware_type=pi_sys_info.get_cpu_hardware_type())
+
+
+@app.context_processor
+def cpu_serial_number():
+    return dict(cpu_serial_number=pi_sys_info.get_cpu_serial_number())
+
+
+@app.context_processor
+def cpu_revision():
+    return dict(cpu_revision=pi_sys_info.get_cpu_revision())
 
 
 @app.context_processor
 def cpu_core_frequency():
-    core_frequency = int(int(subprocess.check_output("vcgencmd measure_clock arm | cut -d= -f2", shell=True).decode().replace('\"', '')) // 1e6)
-    return dict(cpu_core_frequency=core_frequency)
+    return dict(cpu_core_frequency=pi_sys_info.get_cpu_core_frequency())
 
 
 @app.context_processor
-def cpu_core_volt():
-    core_volt = subprocess.check_output("vcgencmd measure_volts| cut -d= -f2", shell=True).decode().replace('\"', '').strip()[:-1]
-    return dict(cpu_core_volt=f"{float(core_volt): .3f}")
+def cpu_core_count():
+    return dict(cpu_core_count=pi_sys_info.get_cpu_core_count())
+
+
+@app.context_processor
+def cpu_core_voltage():
+    return dict(cpu_core_voltage=f"{pi_sys_info.get_cpu_core_voltage(): .3f}")
+
+
+@app.context_processor
+def cpu_usage():
+    return dict(cpu_usage=pi_sys_info.get_cpu_usage())
 
 
 @app.context_processor
 def cpu_temperature():
-    cpuInfo = {'temperature': 0, 'color': 'white'}
-    try:
-        cpuTemp = float(subprocess.check_output(["vcgencmd measure_temp"], shell=True).decode().split('=')[1].split('\'')[0])
-        cpuInfo['temperature']=cpuTemp
-        if cpuTemp > 40 and cpuTemp < 50:
-            cpuInfo['color'] = 'orange'
-        elif cpuTemp > 50:
-            cpuInfo['color'] = 'red'
-        return cpuInfo
-    except Exception as ex:
-        print(ex)
-    finally:
-        return dict(cpu_temperature=cpuInfo)
+    temperature = pi_sys_info.get_cpu_temperature()
+    color = 'white'
+    if 40 < temperature < 50:
+        color = 'orange'
+    elif temperature >= 50:
+        color = 'red'
+    return dict(cpu_temperature={'temperature': temperature, 'color': color})
+
+
+@app.context_processor
+def pi_hostname():
+    return dict(pi_hostname=pi_sys_info.get_hostname())
+
+
+@app.context_processor
+def ram_info():
+    return dict(ram_info=pi_sys_info.get_ram_info())
+
+
+@app.context_processor
+def pi_model():
+    return dict(pi_model=pi_sys_info.get_model())
+
+
+@app.context_processor
+def pi_os():
+    return dict(pi_os=pi_sys_info.get_os_name())
+
+
+@app.context_processor
+def disk_usage_info():
+    return dict(disk_usage_info=pi_sys_info.get_disk_usage_info())
+
+
+@app.context_processor
+def running_process_info():
+    return dict(running_process_info=pi_sys_info.get_running_process_info())
 
 
 @app.context_processor
